7f503ccd23c6cfe61314361579d3643af7192b37
Łukasz Urbański <lukasz.urbanski@student.uj.edu.pl>
2021-03-11 12:22:15 +0100

    Add utils
    Utils are commonly used basic functions and constants

---
+++ api/src/utils/functions.ts
@@ -0,0 +1,15 @@
+export function asArray<T>(v: T | T[]): T[] {
+    return Array.isArray(v) ? v : [v];
+}
+
+export function toStringArray(data: string, delimiter: string): string[] {
+    return data.split(delimiter);
+}
+
+export function toNumber(data: string,): string | number {
+    return isNaN(Number(data)) ? data : Number(data);
+}
+
+export function randomFromRange(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
